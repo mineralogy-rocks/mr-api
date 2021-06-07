cd73b8933d06f04fbaf1792f1d7966b0190b6896
liubomyrgavryliv <liubomyr.gavryliv@gmail.com>
2021-06-07 23:36:18 +0300

    changed sh script

--- db/01-init.sh
+++ db/01-init.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 set -e
-export PGPASSWORD=$DATABASE_PASSWORD;
-pg_restore -h 127.0.0.1 -p $DATABASE_PORT -U $DATABASE_USER -d $DATABASE_NAME -c ./db/master_dump.sql
+psql "postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" -Fc < ./backup/master_dump.sql
