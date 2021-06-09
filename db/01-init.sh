4b4f8d8dd81f0d64a7916233adcf32875ce2c7dc
liubomyrgavryliv <liubomyr.gavryliv@gmail.com>
2021-06-09 18:42:30 +0300

    changed db init.sh

--- db/01-init.sh
+++ db/01-init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 export PGPASSWORD="$POSTGRES_PASSWORD";
-pg_restore -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -c ./docker-entrypoint-initdb.d/master_dump.sql
-# psql "postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" -Fc < ./backup/master_dump.sql
+docker exec database pg_restore -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -c ./docker-entrypoint-initdb.d/master_dump.sql
+# psql "postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@database:$POSTGRES_PORT/$POSTGRES_DB" -Fc < ./docker-entrypoint-initdb.d/master_dump.sql
+# psql "postgresql://gpminerals:gpmineralsDB@database:5432/postgres" -Fc < ./docker-entrypoint-initdb.d/master_dump.sql
