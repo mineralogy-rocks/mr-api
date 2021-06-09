63acaf4f567961f82c99a69c08a42779d2cd6542
liubomyrgavryliv <liubomyr.gavryliv@gmail.com>
2021-06-09 18:48:40 +0300

    changed folder structure for dev d

--- db/sh/01-init.sh
+++ db/sh/01-init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 export PGPASSWORD="$POSTGRES_PASSWORD";
-docker exec database pg_restore -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -c ./docker-entrypoint-initdb.d/master_dump.sql
+pg_restore -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -c ./backup/master_dump.sql
 # psql "postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@database:$POSTGRES_PORT/$POSTGRES_DB" -Fc < ./docker-entrypoint-initdb.d/master_dump.sql
 # psql "postgresql://gpminerals:gpmineralsDB@database:5432/postgres" -Fc < ./docker-entrypoint-initdb.d/master_dump.sql
