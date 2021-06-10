c8deb15299add49618d96117050fcc20eec0623d
liubomyrgavryliv <liubomyr.gavryliv@gmail.com>
2021-06-10 21:45:02 +0300

    rewritten dump_db.sh

--- db/services/dump_db.sh
+++ db/services/dump_db.sh
@@ -2,6 +2,7 @@
 set -e
 
 # Put your credentials below.
+PGPASSWORD=$POSTGRES_PASSWORD_PROD;
 docker-compose -f docker-compose.yml run --rm --no-deps database \
-pg_dump "host=$POSTGRES_HOST_PROD port=$POSTGRES_PORT_PROD dbname=$POSTGRES_DB_PROD user=$POSTGRES_USER_PROD password=$POSTGRES_PASSWORD_PROD" \
+pg_dump --host=$POSTGRES_HOST_PROD --port=$POSTGRES_PORT_PROD dbname=$POSTGRES_DB_PROD --username=$POSTGRES_USER_PROD \
          --clean --no-owner --no-privileges > ./db/backup/db__$(date +%d.%m.%Y__%H-%M).sql
