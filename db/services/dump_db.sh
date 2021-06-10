b0e94805e26bff98323612c208c3f3435df63a3c
liubomyrgavryliv <liubomyr.gavryliv@gmail.com>
2021-06-10 21:53:15 +0300

    import .dev.env into shell script

--- db/services/dump_db.sh
+++ db/services/dump_db.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/bash
+set -a
+. ../../.dev.env
+set +a
+
 set -e
 
 # Put your credentials below.
 PGPASSWORD=$POSTGRES_PASSWORD_PROD;
+PGDATABASE=$POSTGRES_DB_PROD;
+
 docker-compose -f docker-compose.yml run --rm --no-deps database \
-pg_dump --host=$POSTGRES_HOST_PROD --port=$POSTGRES_PORT_PROD dbname=$POSTGRES_DB_PROD --username=$POSTGRES_USER_PROD \
+pg_dump --host=$POSTGRES_HOST_PROD --port=$POSTGRES_PORT_PROD --username=$POSTGRES_USER_PROD \
          --clean --no-owner --no-privileges > ./db/backup/db__$(date +%d.%m.%Y__%H-%M).sql
