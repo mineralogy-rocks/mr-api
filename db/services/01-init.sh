90a2bfa4c3d8d5c7e99cd070e3dd9731f3fdf270
liubomyrgavryliv <liubomyr.gavryliv@gmail.com>
2021-06-10 21:38:41 +0300

    changed docker-compose version to 3

--- db/services/01-init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-export PGPASSWORD="$POSTGRES_PASSWORD";
-# pg_restore -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -c ./backup/master_dump.sql
-psql "postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" -Fc < ./backup/master_dump.sql
-# psql "postgresql://gpminerals:gpmineralsDB@database:5432/postgres" -Fc < ./docker-entrypoint-initdb.d/master_dump.sql
