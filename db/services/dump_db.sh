183483b3d02585f050df4a82f8f4469a90b57a28
liubomyrgavryliv <liubomyr.gavryliv@gmail.com>
2021-06-10 22:25:25 +0300

    refactor dump_db.sh

--- db/services/dump_db.sh
+++ db/services/dump_db.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/bash
-set -a
-. ../../.dev.env
-set +a
-
+export $(grep -v '^#' ./.prod.env | xargs -d '\n')
 set -e
 
 # Put your credentials below.
-PGPASSWORD=$POSTGRES_PASSWORD_PROD;
-PGDATABASE=$POSTGRES_DB_PROD;
+PGPASSWORD=$POSTGRES_PASSWORD;
+PGDATABASE=$POSTGRES_DB;
+
+echo "$POSTGRES_DB";
 
 docker-compose -f docker-compose.yml run --rm --no-deps database \
-pg_dump --host=$POSTGRES_HOST_PROD --port=$POSTGRES_PORT_PROD --username=$POSTGRES_USER_PROD \
-         --clean --no-owner --no-privileges > ./db/backup/db__$(date +%d.%m.%Y__%H-%M).sql
+pg_dump "host=$POSTGRES_HOST port=$POSTGRES_PORT dbname=$POSTGRES_DB user=$POSTGRES_USER password=$POSTGRES_PASSWORD" \
+--clean --no-owner --no-privileges > ./db/backup/db__$(date +%d.%m.%Y__%H-%M).sql
+
+# pg_dump --host=$POSTGRES_HOST --port=$POSTGRES_PORT --username=$POSTGRES_USER \
+#          --clean --no-password --no-owner --no-privileges > ./db/backup/db__$(date +%d.%m.%Y__%H-%M).sql
+
+
+# unset $(grep -v '^#' ./.prod.env | sed -E 's/(.*)=.*/\1/' | xargs)
