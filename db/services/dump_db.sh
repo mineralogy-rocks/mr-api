4365e69eb58937f9782887425bf7af228aebf593
liubomyrgavryliv <liubomyr.gavryliv@gmail.com>
2021-06-10 21:41:12 +0300

    changed path

--- db/services/dump_db.sh
+++ db/services/dump_db.sh
@@ -2,6 +2,6 @@
 set -e
 
 # Put your credentials below.
-docker-compose -f ../../docker-compose.yml run --rm --no-deps database \
+docker-compose -f docker-compose.yml run --rm --no-deps database \
 pg_dump "host=$POSTGRES_HOST_PROD port=$POSTGRES_PORT_PROD dbname=$POSTGRES_DB_PROD user=$POSTGRES_USER_PROD password=$POSTGRES_PASSWORD_PROD" \
-         --clean --no-owner --no-privileges > ../backup/db__$(date +%d.%m.%Y__%H-%M).sql
+         --clean --no-owner --no-privileges > ./db/backup/db__$(date +%d.%m.%Y__%H-%M).sql
