a388f63f5e5f453bd559c2b2c4ef93964f96f4e7
liubomyrgavryliv <liubomyr.gavryliv@gmail.com>
2021-06-11 15:59:35 +0300

    added restore_db sh to docker container

--- db/services/restore_db.sh
+++ db/services/restore_db.sh
@@ -3,4 +3,6 @@ export $(grep -v '^#' ./.dev.env | xargs -d '\n')
 
 set -e
 
-psql "postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@127.0.0.1:$POSTGRES_PORT/$POSTGRES_DB" -Fc < ./db/backup/master_dump.sql
+pg_restore -h 127.0.0.1 -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -Fc -c ./db/backup/master_dump.sql
+
+# psql "postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@127.0.0.1:$POSTGRES_PORT/$POSTGRES_DB" -Fc < ./db/backup/master_dump.sql
