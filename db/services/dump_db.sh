5fe3732303223dcfa1fd1c54230d9d68157134e5
liubomyrgavryliv <liubomyr.gavryliv@gmail.com>
2021-06-11 16:38:51 +0300

    fetch changes

--- db/services/dump_db.sh
+++ db/services/dump_db.sh
@@ -4,6 +4,6 @@ set -e
 
 docker-compose -f docker-compose.yml run --rm --no-deps database \
     pg_dump "host=$POSTGRES_HOST port=$POSTGRES_PORT dbname=$POSTGRES_DB user=$POSTGRES_USER password=$POSTGRES_PASSWORD" \
-    -F p --clean --no-owner --no-privileges > ./db/backup/master_dump.sql
+    --clean --no-owner --no-privileges > ./db/backup/master_dump.sql
 
 # unset $(grep -v '^#' ./.prod.env | sed -E 's/(.*)=.*/\1/' | xargs)
--- db/services/restore_db.sh
+++ db/services/restore_db.sh
@@ -3,6 +3,4 @@ export $(grep -v '^#' ./.dev.env | xargs -d '\n')
 
 set -e
 
-pg_restore -h 127.0.0.1 -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -Fc -c ./db/backup/master_dump.sql
-
-# psql "postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@127.0.0.1:$POSTGRES_PORT/$POSTGRES_DB" -Fc < ./db/backup/master_dump.sql
+psql "postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@127.0.0.1:$POSTGRES_PORT/$POSTGRES_DB" -Fc < ./backup/master_dump.sql
