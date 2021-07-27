d93266492608574ed22aae33faedba45128915ff
liubomyr.gavryliv <liubomyr.gavryliv@gmail.com>
2021-07-27 19:23:08 +0200

    internal ips -> development

--- src/settings/development.py
+++ src/settings/development.py
@@ -12,6 +12,10 @@ MIDDLEWARE += [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
+INTERNAL_IPS = [
+    '127.0.0.1', '0.0.0.0', 'localhost',
+]
+
 DEBUG_TOOLBAR_CONFIG = {
     'SHOW_TOOLBAR_CALLBACK': lambda _request: DEBUG
 }
