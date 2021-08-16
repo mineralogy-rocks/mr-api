42782ec09db579fbd86126ec0e763d18c58e21ae
liubomyr.gavryliv <liubomyr.gavryliv@gmail.com>
2021-08-16 21:11:12 +0200

    fetch changes

--- src/settings/server.py
+++ src/settings/server.py
@@ -16,7 +16,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL')
-AWS_S3_OBJECT_PARAMETERS = { 'CacheControl': 'max-age=86400', }
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }
 
 AWS_LOCATION = 'static'
 AWS_DEFAULT_ACL = 'public-read'
