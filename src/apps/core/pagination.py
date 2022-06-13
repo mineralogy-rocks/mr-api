5dea009c82e9af3bee3996b95a5fef79e8cc26bd
liubomyr.gavryliv <liubomyr.gavryliv@gmail.com>
2022-06-13 22:27:49 +0200

    added custom pagination

---
+++ src/apps/core/pagination.py
@@ -0,0 +1,26 @@
+# -*- coding: UTF-8 -*-
+from collections import OrderedDict
+
+from rest_framework.response import Response
+from rest_framework.pagination import LimitOffsetPagination
+
+
+class CustomLimitOffsetPagination(LimitOffsetPagination):
+    """
+    Custom paginator which will return the queryset, instead of list
+    """
+
+    def paginate_queryset(self, queryset, request, view=None):
+        self.limit = self.get_limit(request)
+        if self.limit is None:
+            return None
+
+        self.count = self.get_count(queryset)
+        self.offset = self.get_offset(request)
+        self.request = request
+        if self.count > self.limit and self.template is not None:
+            self.display_page_controls = True
+
+        if self.count == 0 or self.offset > self.count:
+            return []
+        return queryset[self.offset:self.offset + self.limit]
