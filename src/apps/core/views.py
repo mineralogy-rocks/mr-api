fix variety formula retrieval

src/apps/core/views.py
@@ -374,7 +374,7 @@ class MineralViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
                 _merge = pd.merge(pd.DataFrame(data), pd.DataFrame(_data), on='id', how='left')
 
                 _merge['_statuses'] = _merge.apply(lambda x: [y['status_id'] for y in x['statuses']], axis=1)
-                _merge['has_formula'] = _merge.apply(lambda x: len(np.intersect1d([0.0, 4.04, 4.05], x['_statuses'])) and x['formulas'], axis=1)
+                _merge['has_formula'] = _merge.apply(lambda x: len(np.intersect1d([0.0, 4.0, 4.05], x['_statuses'])) and x['formulas'], axis=1)
                 _merge['has_crystal_system'] = _merge.apply(lambda x: len(np.intersect1d([0.0, 4.04, 4.05], x['_statuses'])) and x['crystal_systems'], axis=1)
 
                 _drop_columns = ['_statuses', 'has_formula', 'has_crystal_system',]