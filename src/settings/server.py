5df30ef260cf693a6a399f0c2c80fb698de05169
Liubomyr Gavryliv <liubomyr.gavryliv@gmail.com>
2024-10-29 15:11:20 +0100

    remove static root

--- src/settings/server.py
+++ src/settings/server.py
@@ -39,4 +39,4 @@ EMAIL_USE_TLS = True
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 STATIC_URL = "{}/{}/".format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
-STATIC_ROOT = os.path.join(BASE_DIR, "")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
